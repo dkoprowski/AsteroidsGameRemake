41e1c1270a04937f706a51d381da7a2fe2df4166
Daniel Koprowski <dkoprowski@outlook.com>
2016-05-21 10:51:29 +0200

    Calibrated speed of movement and rotation. Added bullet factory "RocketSpawner" class.

---
+++ Assets/Scripts/RocketsSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketsSpawner : MonoBehaviour {
+
+    public Transform RocketsParent;
+    public GameObject RocketPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SpawnRocket(RocketPrefab);
+        }
+	}
+
+    public void SpawnRocket(GameObject prefab)
+    {
+
+        GameObject newRocket = Instantiate(prefab) as GameObject;
+        //  lasers.Add(enemy);
+        newRocket.transform.SetParent(RocketsParent);
+        newRocket.transform.position = transform.position;
+        newRocket.transform.localEulerAngles = transform.localEulerAngles;
+    }
+}
