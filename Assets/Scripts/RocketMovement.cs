c670e3cb55d942f45dea26d558c8a90a666a2896
Daniel Koprowski <dkoprowski@outlook.com>
2016-05-21 10:54:18 +0200

    Added rocket movement

---
+++ Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketMovement : MonoBehaviour {
+    public float RocketSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        MoveForward(RocketSpeed);
+	}
+
+    void MoveForward(float speed)
+    {
+        gameObject.transform.localPosition += transform.up * speed * Time.deltaTime;
+    }
+}
