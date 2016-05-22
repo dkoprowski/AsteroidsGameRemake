c35aaf56d3e0272cb2175b726891f5e9fa631a63
Daniel Koprowski <dkoprowski@outlook.com>
2016-05-22 11:44:23 +0200

    Added :player's life loss behaviour

---
+++ Assets/Scripts/BlinkBehav.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlinkBehav : MonoBehaviour
+{
+    public Collider2D colliderToCheck;
+    public SpriteRenderer spriteToBlink;
+    public float blinkCycleTime;
+    private float nextBlink;
+    private void Start()
+    {
+        
+    }
+    private void Update()
+    {
+        if (!colliderToCheck.enabled)
+        {
+            if (Time.time > nextBlink)
+            {
+                Blink();
+                nextBlink = (Time.time + blinkCycleTime);
+            }
+        }
+        else
+        {
+            if (spriteToBlink.color.a < 1f)
+            {
+                spriteToBlink.color = new Color(spriteToBlink.color.r, spriteToBlink.color.g, spriteToBlink.color.b, 1f);
+            }
+        }
+    }
+
+    private void Blink()
+    {
+        spriteToBlink.color = new Color(spriteToBlink.color.r, spriteToBlink.color.g, spriteToBlink.color.b, 1f - spriteToBlink.color.a);
+    }
+}
--- Assets/Scripts/PlayerBehav.cs
+++ Assets/Scripts/PlayerBehav.cs
@@ -4,8 +4,44 @@ using System.Collections;
 public class PlayerBehav : MonoBehaviour {
 
     public PointsController points;
+    private Rigidbody2D _rigidbody2D;
+    private Collider2D _collider2D;
+    public float PlayerRestTime;
+    public float realRestTime;
+    private void Start()
+    {
+        _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+        _collider2D = gameObject.GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (!_collider2D.enabled)
+        {
+            realRestTime += Time.deltaTime;
+            if(realRestTime >= PlayerRestTime)
+            {
+                _collider2D.enabled = true;
+            }
+        }
+        else
+        {
+            realRestTime = 0f;
+        }
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
         points.RemoveLife();
+        RespawnPlayer();
+    }    
+
+    private void RespawnPlayer()
+    {
+        transform.localPosition = new Vector3(0,0,0);
+        transform.localEulerAngles = new Vector3(0, 0, 0);
+        _rigidbody2D.velocity = new Vector2(0, 0);
+        _rigidbody2D.angularVelocity = 0f;
+        _collider2D.enabled = false;
     }
 }
