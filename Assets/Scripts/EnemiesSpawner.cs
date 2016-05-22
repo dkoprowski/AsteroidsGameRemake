22b777a9d546db17d4e2c7bcb4512b7997e974f4
Daniel Koprowski <dkoprowski@outlook.com>
2016-05-22 12:56:50 +0200

    Added three types of enemies

--- Assets/Scripts/EnemiesSpawner.cs
+++ Assets/Scripts/EnemiesSpawner.cs
@@ -7,6 +7,8 @@ public class EnemiesSpawner : MonoBehaviour {
     public float DirectionUpperLimit;
     public float DirectionBottomLimit;
     public GameObject EnemyPrefab;
+    public GameObject EnemyPrefab2;
+    public GameObject EnemyPrefab3;
     public Transform EnemiesParent;
     public float SpawnFrequency;
     private float nextSpawn;
@@ -16,17 +18,32 @@ public class EnemiesSpawner : MonoBehaviour {
         if (Time.time > nextSpawn)
         {
 
-            CreateEnemy(EnemyPrefab);
+            CreateEnemy(RandomEnemy());
 
             nextSpawn = Time.time + SpawnFrequency;
         }
+
+        if(SpawnFrequency > 1f)
+            SpawnFrequency = SpawnFrequency - (Time.deltaTime * 0.03f);
     }
 
     private void CreateEnemy(GameObject EnemyPrefab)
     {
+
         GameObject newEnemy = Instantiate(EnemyPrefab) as GameObject;
         newEnemy.transform.SetParent(EnemiesParent);
         newEnemy.transform.position = transform.position;
         newEnemy.transform.localEulerAngles = new Vector3(0,0,UnityEngine.Random.Range(DirectionBottomLimit, DirectionUpperLimit));
+    }
+
+    private GameObject RandomEnemy()
+    {
+        float random = UnityEngine.Random.Range(0f, 1f);
+        if (random > 0.8f)
+            return EnemyPrefab3;
+        else if (random > 0.5f)
+            return EnemyPrefab2;
+        else
+            return EnemyPrefab;
     }
 }
