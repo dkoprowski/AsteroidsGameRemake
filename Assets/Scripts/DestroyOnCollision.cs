9bbf7825aca1b7f459966104f0edd5efb2bbe0c7
Daniel Koprowski <dkoprowski@outlook.com>
2016-05-22 14:10:46 +0200

    Added explosions particles

--- Assets/Scripts/DestroyOnCollision.cs
+++ Assets/Scripts/DestroyOnCollision.cs
@@ -4,15 +4,22 @@ using System.Collections;
 public class DestroyOnCollision : MonoBehaviour
 {
     public PointsController pointsController;
-
+    public GameObject explosionPrefab;
+    public GameObject explosionsParent;
     public void Start()
     {
         pointsController = FindObjectOfType<PointsController>();
+        explosionsParent = GameObject.Find("ExplosionsParent");
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag != "Player")
             Destroy(collision.gameObject);
+
+        var explosion = Instantiate(explosionPrefab) as GameObject;
+        explosion.transform.SetParent(explosionsParent.transform);
+        explosion.transform.localPosition = new Vector3(gameObject.transform.localPosition.x, gameObject.transform.localPosition.y, explosion.transform.localPosition.z);
+
         Destroy(gameObject);
         pointsController.AddPoint();
     }
---
+++ Assets/Scripts/ExplosionDestroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExplosionDestroy : MonoBehaviour {
+
+    public float LifeTime = 2f;
+    private float CreateTime;
+
+	void Start () {
+        CreateTime = Time.time;
+	}
+	
+	void Update () {
+        if (Time.time > CreateTime + LifeTime)
+            Destroy(gameObject);
+	}
+}
